5d1ee94a28c7f97bee7bcba9e5a9595d9c476992
anyang <anyang@baidu.com>
2019-03-05 17:41:10 +0800

    modify jacoco

--- travis/java/after_script.sh
+++ travis/java/after_script.sh
@@ -4,7 +4,7 @@ set -ev
 pushd agent/java
 mkdir temp
 cp -r boot/target/classes temp
-cp -r engine/target/classes/com/baidu/openrasp/* tmp/classes/com/baidu/openrasp
+cp -r engine/target/classes/com/baidu/openrasp/* temp/classes/com/baidu/openrasp
 pushd integration-test/jacoco
 dataFile=/home/travis/build/baidu/openrasp/agent/java
 java -jar jacococli.jar report $dataFile/jacoco.exec --classfiles ../../temp/classes/ --xml jacoco.xml
