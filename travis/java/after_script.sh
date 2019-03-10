640b471b6dab6463d5a12ad33c7d32601454748d
anyang <anyang@baidu.com>
2019-03-10 14:28:35 +0800

    modify jacoco

--- travis/java/after_script.sh
+++ travis/java/after_script.sh
@@ -22,8 +22,8 @@ if [[ -f "rasp-engine.jar" ]] && [[ -f "rasp.jar" ]]; then
     cp -r com/ ../classes
     popd
     rm -rf integration-test/jacoco/sources/com/baidu/openrasp/*
-    cp -R engine/src/main/java/com/baidu/openrasp/* integration-test/jacoco/sources/com/baidu/openrasp/
-    cp -R boot/src/main/java/com/baidu/openrasp/* integration-test/jacoco/sources/com/baidu/openrasp/
+    cp -r engine/src/main/java/com/baidu/openrasp/* integration-test/jacoco/sources/com/baidu/openrasp/
+    cp -r boot/src/main/java/com/baidu/openrasp/* ntegration-test/jacoco/sources/com/baidu/openrasp/
 fi
 pushd integration-test/jacoco
 dataFile=/home/travis/build/baidu/openrasp/agent/java
