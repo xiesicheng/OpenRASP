96c972b7e65e34c5719116b65b5e0fa662a9fb03
anyang <anyang@baidu.com>
2019-04-10 19:26:06 +0800

    modify ci

--- travis/java/script.sh
+++ travis/java/script.sh
@@ -2,6 +2,7 @@
 set -ev
 
 source /tmp/openrasp_java_server_home.sh
+cat agent/java/integration-test/nohup.out
 pushd agent/java/integration-test/tester
 node_modules/.bin/mocha *.test.js -t 30000 --retries 3
 popd
