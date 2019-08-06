1068cc580b73fa89b396ceb806e8ad862b69addf
Li <lixinkai@baidu.com>
2019-08-06 17:24:27 +0800

    [PHP7]explode

--- agent/php7/taint/hook.cc
+++ agent/php7/taint/hook.cc
@@ -22,6 +22,7 @@
  * taint 相关hook点
  */
 POST_HOOK_FUNCTION(strval, TAINT);
+POST_HOOK_FUNCTION(explode, TAINT);
 
 void post_global_strval_TAINT(OPENRASP_INTERNAL_FUNCTION_PARAMETERS)
 {
@@ -32,4 +33,70 @@ void post_global_strval_TAINT(OPENRASP_INTERNAL_FUNCTION_PARAMETERS)
         WRONG_PARAM_COUNT;
     }
     str_unchanege_taint(*arg, return_value);
+}
+
+void post_global_explode_TAINT(OPENRASP_INTERNAL_FUNCTION_PARAMETERS)
+{
+    int size = 0;
+    if (Z_TYPE_P(return_value) == IS_ARRAY && (size = zend_hash_num_elements(Z_ARRVAL_P(return_value))) > 0)
+    {
+        zval *zdelim = nullptr;
+        zval *zstr = nullptr;
+        long limit = ZEND_LONG_MAX; /* No limit */
+
+        if (zend_parse_parameters(ZEND_NUM_ARGS(), "zz|l", &zdelim, &zstr, &limit) == FAILURE)
+        {
+            return;
+        }
+
+        if (limit == 0)
+        {
+            limit = 1;
+        }
+        if (Z_TYPE_P(zstr) == IS_STRING && Z_STRLEN_P(zstr) &&
+            Z_TYPE_P(zdelim) == IS_STRING && Z_STRLEN_P(zdelim) &&
+            openrasp_taint_possible(zstr))
+        {
+            NodeSequence ns = openrasp_taint_sequence(zstr);
+            std::string str(Z_STRVAL_P(zstr), Z_STRLEN_P(zstr));
+            std::string delim(Z_STRVAL_P(zdelim), Z_STRLEN_P(zdelim));
+            size_t start = 0;
+            size_t found = 0;
+
+            zval *val;
+            zend_string *key;
+            zend_ulong idx;
+            ZEND_HASH_FOREACH_KEY_VAL(Z_ARRVAL_P(return_value), idx, key, val)
+            {
+                if (Z_TYPE_P(val) == IS_STRING && nullptr == key)
+                {
+                    if (idx < size - 1)
+                    {
+                        found = str.find(delim, start);
+                        if (found != std::string::npos)
+                        {
+                            openrasp_taint_mark(val, new NodeSequence(ns.sub(start, found - start)));
+                            start = found + delim.length();
+                        }
+                    }
+                    else if (idx == size - 1)
+                    {
+                        if (limit > 0)
+                        {
+                            openrasp_taint_mark(val, new NodeSequence(ns.sub(start)));
+                        }
+                        else
+                        {
+                            found = str.find(delim, start);
+                            if (found != std::string::npos)
+                            {
+                                openrasp_taint_mark(val, new NodeSequence(ns.sub(start, found - start)));
+                            }
+                        }
+                    }
+                }
+            }
+            ZEND_HASH_FOREACH_END();
+        }
+    }
 }
