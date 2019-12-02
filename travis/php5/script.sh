cfcfca5afd00509c7b75d5d76fd7c39b58113cd8
Li <lixinkai@baidu.com>
2019-12-02 21:02:31 +0800

    test

--- travis/php5/script.sh
+++ travis/php5/script.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -ev
 
+ulimit -c unlimited -S
+echo '/tmp/core.%p.%E' > /proc/sys/kernel/core_pattern
+
 pushd agent/$OPENRASP_LANG
 php run-tests.php -p `which php` -d extension=`pwd`/modules/openrasp.so --offline --show-diff --set-timeout 120
 popd
+
+for i in $(find /tmp -maxdepth 1 -name 'core*' -print); do gdb php-cgi core* -ex "thread apply all bt" -ex "set pagination 0" -batch; done;
