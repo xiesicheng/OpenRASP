b19e4311655df097e6a0ddd0f83ade9befd9ff2e
Li <lixinkai@baidu.com>
2019-12-02 21:10:44 +0800

    test

--- travis/php5/script.sh
+++ travis/php5/script.sh
@@ -2,7 +2,7 @@
 set -ev
 
 ulimit -c unlimited -S
-echo '/tmp/core.%p.%E' > /proc/sys/kernel/core_pattern
+sudo bash -c "echo '/tmp/core.%p.%E' > /proc/sys/kernel/core_pattern"
 
 pushd agent/$OPENRASP_LANG
 php run-tests.php -p `which php` -d extension=`pwd`/modules/openrasp.so --offline --show-diff --set-timeout 120
